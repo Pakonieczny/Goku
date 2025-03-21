88fae9621dcebb08d247ea6b412be319f05f3e01
Pakonieczny <pakonieczny@gmail.com>
2025-03-21 16:42:15 -0400

    Add files via upload

---
+++ uiPositions.js
@@ -0,0 +1,25 @@
+// uiPositions.js
+// Default UI positions for your elements.
+// These values will be used only if no local positions exist.
+window.defaultUIPositions = {
+  configureButtonsBtn: { top: "10px", left: "10px" },
+  listingDescContainer: { top: "20px", left: "20px" },
+  uploadedFilesList: { top: "20px", left: "540px" },
+  dropZone: { top: "150px", left: "10px" },
+  previewGridContainer: { top: "300px", left: "10px" },
+  container13: { top: "-150px", left: "10px" },
+  uploadToEtsyBtn: { top: "50px", left: "10px" },
+  etsyShopListingsContainer: { top: "50px", left: "200px" },
+  userProvidedKeyPhrasesContainer: { top: "125px", left: "10px" },
+  etsyListingTitleContainer: { top: "-50px", left: "10px" },
+  etsyListingDescriptionContainer: { top: "0px", left: "10px" },
+  regenTitleBtn: { top: "0px", left: "0px" },
+  copyTitleBtn: { top: "0px", left: "130px" },
+  regenDescriptionBtn: { top: "0px", left: "0px" },
+  copyDescriptionBtn: { top: "0px", left: "130px" },
+  photoUploadNewBtn: { top: "10px", left: "10px" },
+  generateBtn: { top: "10px", left: "150px" },
+  connectEtsyBtn: { top: "10px", left: "300px" },
+  analyzeMetadataBtn: { top: "10px", left: "450px" },
+  showAnalyzeRulesBtn: { top: "10px", left: "600px" }
+};
