c287723eacb2b495bb843e17ed02ec493739e972
Pakonieczny <pakonieczny@gmail.com>
2026-05-25 17:32:56 -0400

    Delete netlify/functions directory

--- netlify/functions/geminiImageProxy.js
+++
@@ -1,8 +0,0 @@
-/* netlify/functions/geminiImageProxy.js
-   Synchronous wrapper so the browser can receive JSON.
-   (Background functions return immediately and may return an empty body.)
-*/
-
-const impl = require("./geminiImageProxy-background");
-
-exports.handler = impl.handler;
